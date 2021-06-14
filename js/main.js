57c4d365ce1776256d6c633bbfbf2340adf222fa
Igor Trishin <i.a.trish65@gmail.com>
2021-06-14 22:59:05 +0700

    Исправление нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-//первая функция
-
-let accountRoom = function() {
-  let daysRoom = 5;
-  let roomPrice = 100;
-  let accountRoom = daysRoom * roomPrice;
-
-  console.log("Стоимость проживания составит за " + daysRoom + " дней: " + accountRoom + " рублей.");
-
-  return accountRoom;
-
-    if (daysRoom => roomPrice) {
-     return accountRoom*0.5;
-    };
-};
-  //accoutRoom();
-
-//вторая функция.
-
-function sumNamber(min, max, maxDigits = 0) {
-    if (min > max || min < 0 || max <= 0) {
-      return ('Задан неверный диапазон! Укажите другие числа.');
-    }
-
-    const digitsDegree = 3 ** maxDigits;
-    return ((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+function randomDigit (min, max) {
+  if (min >= max || min < 0 || max <= 0) {
+    throw new Error('Диапазон включает только положительные числа. Число "от" не может быть больше числа "до".');
   }
-  //sumNamber(1, 2, 6);
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+function randomDigitPoint (min, max, n) {
+  if (min >= max || min < 0 || max <= 0) {
+    throw new Error('Диапазон включает только положительные числа. Число "от" не может быть больше числа "до".');
+  }
+
+  if (n < 0) {
+    throw new Error('Значение должно быть больше или равно 0');
+  }
+
+  if ((n ^ 0) !== n) {
+    throw new Error('Значение должно быть целым числом');
+  }
+
+  const decimalPoint = Math.pow(10, n);
+  const random = +((min + Math.random() * (max - min)).toFixed(n));
+  return Math.floor((random) * decimalPoint) / decimalPoint;
+}
