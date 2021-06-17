ad8b7876c35ad5b650fa07dd83192729389e067a
Igor Trishin <i.a.trish65@gmail.com>
2021-06-17 20:04:57 +0700

    Вторичная правка 2.14

--- js/main.js
+++ js/main.js
@@ -9,22 +9,24 @@ function randomDigit (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+randomDigit (1, 5);
 
-
-function randomDigitPoint (min, max, n) {
+function randomDigitPoint (min, max, nut) {
   if (min >= max || min < 0 || max <= 0) {
     throw new Error('Диапазон включает только положительные числа. Число "от" не может быть больше числа "до".');
   }
 
-  if (n < 0) {
+  if (nut < 0) {
     throw new Error('Значение должно быть больше или равно 0');
   }
 
-  if ((n ^ 0) !== n) {
+  if ((nut ^ 0) !== nut) {
     throw new Error('Значение должно быть целым числом');
   }
 
-  const decimalPoint = Math.pow(10, n);
-  const random = +((min + Math.random() * (max - min)).toFixed(n));
+  const decimalPoint = Math.pow(10, nut);
+  const random = +((min + Math.random() * (max - min)).toFixed(nut));
   return Math.floor((random) * decimalPoint) / decimalPoint;
 }
+
+randomDigitPoint (1, 9, 5);
